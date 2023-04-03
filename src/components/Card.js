f279ec540e2765e91ee21cdc9dfd0654d4abe14c
Jose Garrera <josegarrera@gmail.com>
2023-04-03 06:16:32 -0300

    add width and height to image

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,6 +6,8 @@ const Card = ({ name, email, id }) => {
       <img
         alt="robots"
         src={`https://robohash.org/${id}?size=200x200`}
+        width={200}
+        height={200}
       />
       <div>
         <h2>{name}</h2>
